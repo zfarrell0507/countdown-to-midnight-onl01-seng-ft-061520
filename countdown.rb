ffcd393b33bf4dc22a19ed8b49f47aaa70c9bc88
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-28 17:21:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 
-def countdown
-  number = 12
+def countdown(num = 10)
+  number = num
   
   while number >= 0
     puts "#{number} SECOND(S)"
@@ -21,5 +21,5 @@ def countdown_with_sleep(num = 10)
   end
   puts "HAPPY NEW YEAR!"
 end
-countdown
-countdown_with_sleep
+countdown(12)
+countdown_with_sleep(10)
